23726237c58d05746f27ecd0c85e4c950df35e4d
Saptarshi Chakraborty <c7rishi@ufl.edu>
2018-06-03 09:48:09 -0400

    changed example for bestmodel

--- R/all_model_select.R
+++ R/all_model_select.R
@@ -430,7 +430,7 @@ fit_incremental_angmix <- function(model, data, crit = "LOOIC",
 #' @examples
 #' # illustration only - more iterations needed for convergence
 #' set.seed(1)
-#' fit.vmsin.step.15 <- fit_incremental_angmix("vmsin", tim8, start_ncomp = 3,
+#' fit.vmsin.step.15 <- fit_incremental_angmix("vmsin", tim8, start_ncomp = 1,
 #'                                             max_ncomp = 3, n.iter = 15,
 #'                                             n.chains = 1)
 #' fit.vmsin.best.15 <- bestmodel(fit.vmsin.step.15)
