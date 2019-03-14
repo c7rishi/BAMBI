2690e47c8f1e079dfc45eb23b9a5cb22fbe1beb2
Saptarshi Chakraborty <c7rishi@ufl.edu>
2019-03-14 10:08:04 -0400

    changed to crit = "WAIC" as LOOIC breaks with small MCMC iteration size.

--- R/all_model_select.R
+++ R/all_model_select.R
@@ -482,7 +482,8 @@ fit_incremental_angmix <- function(model, data,
 #' set.seed(1)
 #' fit.vmsin.step.15 <- fit_incremental_angmix("vmsin", tim8, start_ncomp = 1,
 #'                                             max_ncomp = 3, n.iter = 15,
-#'                                             n.chains = 1)
+#'                                             n.chains = 1,
+#'                                             crit = "WAIC")
 #' fit.vmsin.best.15 <- bestmodel(fit.vmsin.step.15)
 #' fit.vmsin.best.15
 #'
